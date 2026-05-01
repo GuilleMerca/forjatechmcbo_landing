79b6106595f9b53680d1ea99772ef91b7506a0d3
GuilleMerca <guillermomercado.usa@gmail.com>
2026-05-01 09:26:23 -0400

    mejoras y redundancias e implementacion de imagenes con extensiones

---
+++ env.js
@@ -0,0 +1,6 @@
+const WA_NUMBER = "584246555157";
+const EMAILJS_SERVICE_ID = "service_qqgy6en";
+const EMAILJS_TEMPLATE_ID = "template_k9xpbzu";
+const EMAILJS_USER_ID = "pYRjEaqZQViCKwmnA";
+const MERCADOLIBRE_LINK =
+  "https://www.mercadolibre.com.ve/pagina/guillermomercado_gj";
